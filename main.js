f68883722cd6857d5dc0b57ccc75c6bab4b5437e
sina mansoory <sinaman7979@gmail.com>
2022-02-03 19:03:21 -0800

    add an envent to equal sign and add function calculate to show the result

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-const numbers = document.querySelectorAll('.num-btn');
-const resultBox = document.querySelector('.container').querySelector('.result').querySelector("#result-box");
+"use strict";
 
-numbers.forEach(items => {
+const buttons = document.querySelectorAll('.show-display');
+const resultBox = document.querySelector('.container').querySelector('.result').querySelector("#result-box");
+document.getElementsByClassName('equal')[0].addEventListener('click', calculate);
+
+buttons.forEach(items => {
     items.addEventListener('click', showNumbers)
 })
 
 function showNumbers(event) {
     const num = event.target.innerText;
-    if (resultBox.innerHTML === 0) {
-        return resultBox.innerHTML = num;
+    if (resultBox.innerHTML == 0) {
+       resultBox.innerHTML = num;
+    } else {
+        resultBox.innerHTML += num;
     }
         
+}
+
+function calculate() {
+    const result = resultBox.innerHTML;
+    resultBox.innerHTML =  eval(result);
 }
